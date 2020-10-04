c60f468c89e35a977c71a55180d0c4ba68b3fc1a
Neb <nebulom@gmail.com>
2020-10-04 12:16:42 +0800

    Added styles for chapter show

--- application/views/home/chapter.php
+++ application/views/home/chapter.php
@@ -1,11 +1,20 @@
-<h3>
+<h3 class="title">
   <?php echo anchor('book/' . $chapter->book_id, $chapter->book_name); ?>
   <?php echo $chapter->title; ?>
 </h3>
 
 <?php foreach ($verses as $verse): ?>
-  <p>
-    <?php echo $verse->number; ?>
+  <p class="verse">
+    <sup><?php echo $verse->number; ?></sup>
     <?php echo $verse->content; ?>
   </p>
 <?php endforeach; ?>
+
+<style>
+  .verse {
+    text-indent: 30px;
+  }
+  .title {
+    text-align: center;
+  }
+</style>
